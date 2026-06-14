1d64665ab1ee8de07139c08e42e0e7f4e084248f
femboytoucher504 <ahmemuhsins3169@gmail.com>
2026-06-14 14:38:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,144 +1,57 @@
-const findByProps = (...props) => {
-    const modules = window.__revenge_modules || [];
-    for (const m of Object.values(modules)) {
-        if (m?.exports && props.every(p => p in m.exports)) return m.exports;
-        if (m?.exports?.default && props.every(p => p in m.exports.default)) return m.exports.default;
-    }
-    return null;
-};
+"use strict";
 
-const storage = {};
+Object.defineProperty (exports, "_esModule", { value: true
 
-const EMOTE_SIZE_KEY = "emote_size";
-const FORMAT_TYPE_KEY = "format_type";
-const REALMOJI_KEY = "realmoji";
-const COMPOUND_SENTENCES_KEY = "compound_sentences";
-const USE_WEBP_KEY = "use_webp";
-const EMOTE_SIZE_DEFAULT = "48";
-const FORMAT_TYPE_DEFAULT = "markdown";
-const REALMOJI_DEFAULT = false;
-const COMPOUND_SENTENCES_DEFAULT = false;
-const USE_WEBP_DEFAULT = false;
-const FORMAT_EXT_MD = "markdown_ext";
-const FORMAT_MD = "markdown";
+});
 
-function getSetting(key, def) {
-    const v = storage[key];
-    return v === undefined ? def : v;
+exports.onUnload = void 0;
+
+var _patcher = require("@vendetta/patcher");
+
+var _metro = require("@vendetta/metro");
+
+var _plugin = require("@vendetta/plugin");
+
+var _findByProps, _findByProps2;
+
+const EmojiUtils = (_findByProps = (0, _metro.
+
+findByProps) ("isEmojiUsable")) !== null && _findByProps !== void 0? _findByProps: (0, _metro.findByProps) ("isUsableEmoji");
+
+const EmojiInfo = (_findByProps2 = (0, metro. findByProps) ("getChatInputText")) !== null && _findByProps2 !== void 0? _findByProps2: (0, _metro.findByProps) ("getEmojiChatInputText"); const patches = [];
+
+if (EmojiUtils) {
+
 }
 
-function buildEmojiUrl(emojiId, emoteName, isAnimated) {
-    const useWebp = getSetting(USE_WEBP_KEY, USE_WEBP_DEFAULT);
-    const emoteSize = parseInt(getSetting(EMOTE_SIZE_KEY, EMOTE_SIZE_DEFAULT), 10) || 48;
-    let url = "https://cdn.discordapp.com/emojis/" + emojiId;
-    if (useWebp) {
-        url += ".webp?name=" + emoteName + "&lossless=true";
-        if (isAnimated) url += "&animated=true";
-    } else {
-        url += isAnimated ? ".gif" : ".png";
-        url += "?name=" + emoteName;
-    }
-    url += "&size=" + emoteSize;
-    return url;
+const key = "isEmojiUsable" in EmojiUtils ? "isEmojiUsable": "isUsableEmoji"; patches.push((0, _patcher.instead) (key, EmojiUtils, () => true));
+
+if (EmojiInfo) {
+
 }
 
-function formatOutput(url, emoteName) {
-    const fmt = getSetting(FORMAT_TYPE_KEY, FORMAT_TYPE_DEFAULT);
-    if (fmt === FORMAT_EXT_MD) return "[\u2236" + emoteName + "\u2236](" + url + ")";
-    if (fmt === FORMAT_MD) return "[" + emoteName + "](" + url + ")";
-    return url;
-}
+const key = "getChatInputText" in EmojiInfo ? "getChatInputText": "getEmojiChatInputText"; patches.push((0, _patcher.instead) (key, EmojiInfo, (args, orig) => {
 
-const emojiRegex = /<(a)?:(F_)?([a-zA-Z0-9_]+):(\d+)>/g;
-const markdownRegexCompound = /(?:\[[\u2236\[]?[^\]]*[\u2236\]]?\]\()?(https:\/\/cdn\.discordapp\.com\/emojis\/(\d+)\.(gif|png|webp)[^)\s]*)\)?/g;
-const markdownRegexSingle = /^(?:\[[\u2236\[]?[^\]]*[\u2236\]]?\]\()?(https:\/\/cdn\.discordapp\.com\/emojis\/(\d+)\.(gif|png|webp)[^)\s]*)\)?$/;
+var _ref, _emoji$id, _emoji$name, _ref2, _emoji$animated;
 
-function cdnUrlToMention(url, emojiId, extension) {
-    let animated = extension === "gif" ? "a" : "";
-    let emojiName = "UNKNOWN_FAKE_EMOJI";
-    try {
-        new URL(url).searchParams.forEach(function(val, key) {
-            if (key === "name") emojiName = val.replace(/[^a-zA-Z0-9_]/g, "");
-            if (key === "animated" && val === "true" && extension === "webp") animated = "a";
-        });
-    } catch(e) {}
-    return "<" + animated + ":" + emojiName + ":" + emojiId + ">";
-}
+const emoji = args[0];
 
-const patches = [];
+if (!emoji || emoji.available && emoji. isUsable) return orig(...args);
 
-module.exports = {
-    onLoad: function() {
-        const Patcher = findByProps("instead", "before", "after");
-        const EmojiUtils = findByProps("isEmojiUsable", "getEmojiUrl")
-            || findByProps("isUsableEmoji", "getCustomEmojiURL")
-            || findByProps("isEmojiUsable");
+const id = (_ref = (_emoji$id = emoji.id) !== null && _emoji$id !== void 0?
 
-        if (EmojiUtils && Patcher) {
-            const usableKey = "isEmojiUsable" in EmojiUtils ? "isEmojiUsable" : "isUsableEmoji";
-            patches.push(Patcher.instead(usableKey, EmojiUtils, function() { return true; }));
-            if ("canUseEmoji" in EmojiUtils) {
-                patches.push(Patcher.instead("canUseEmoji", EmojiUtils, function() { return true; }));
-            }
-        }
+_emoji$id: emoji.idStr) !== null && _ref !== void 0? _ref: "";
 
-        const EmojiInfo = findByProps("getChatInputText") || findByProps("getEmojiChatInputText");
-        if (EmojiInfo && Patcher) {
-            const chatKey = "getChatInputText" in EmojiInfo ? "getChatInputText" : "getEmojiChatInputText";
+const name = (_emoji$name = emoji.name) !== null && _emoji$name !== void 0? _emoji$name: "";
 
-            const patchFn = function(args, orig) {
-                const emoji = args[0];
-                if (!emoji) return orig.apply(this, args);
-                if (emoji.available && emoji.isUsable) return orig.apply(this, args);
-                const emojiId = emoji.id || emoji.idStr || "";
-                const emoteName = emoji.name || "";
-                const isAnimated = emoji.animated || emoji.isAnimated || false;
-                if (!emojiId) return orig.apply(this, args);
-                if (getSetting(REALMOJI_KEY, REALMOJI_DEFAULT)) {
-                    return "<" + (isAnimated ? "a" : "") + ":F_" + emoteName + ":" + emojiId + ">";
-                }
-                return formatOutput(buildEmojiUrl(emojiId, emoteName, isAnimated), emoteName);
-            };
+const animated = (_ref2 = (_emoji$animated = emoji.animated) !== null && _emoji$animated !== void 0?
 
-            patches.push(Patcher.instead(chatKey, EmojiInfo, patchFn));
-            if ("getMessageContentReplacement" in EmojiInfo) {
-                patches.push(Patcher.instead("getMessageContentReplacement", EmojiInfo, patchFn));
-            }
-        }
+_emoji$animated emoji.isAnimated) !== null && _ref2 !== void 0?ref2 false; if (!id) return orig(...args); const url = 'https://cdn.discordapp.com/emojis/${id}${animated ? ".gif" ".png"}? name=${name}&size=48`;
 
-        if (getSetting(REALMOJI_KEY, REALMOJI_DEFAULT)) {
-            const MessageStore = findByProps("receiveMessage", "getMessage");
-            if (MessageStore && Patcher) {
-                patches.push(Patcher.before("receiveMessage", MessageStore, function(args) {
-                    const msg = args[1] || args[0];
-                    if (!msg || typeof msg.content !== "string") return;
-                    if (getSetting(COMPOUND_SENTENCES_KEY, COMPOUND_SENTENCES_DEFAULT)) {
-                        msg.content = msg.content.replace(markdownRegexCompound, function(_, url, id, ext) {
-                            return cdnUrlToMention(url, id, ext);
-                        });
-                    } else {
-                        const m = msg.content.match(markdownRegexSingle);
-                        if (m) msg.content = cdnUrlToMention(m[1], m[2], m[3]);
-                    }
-                }));
-            }
+return [${name}](${url})`;
 
-            const RestAPI = findByProps("sendMessage", "editMessage");
-            if (RestAPI && Patcher) {
-                patches.push(Patcher.before("sendMessage", RestAPI, function(args) {
-                    const msgObj = args[1];
-                    if (!msgObj || typeof msgObj.content !== "string") return;
-                    msgObj.content = msgObj.content.replace(emojiRegex, function(full, anim, fake, name, id) {
-                        if (fake !== "F_") return full;
-                        return "[" + name + "](" + buildEmojiUrl(id, name, anim === "a") + ")";
-                    });
-                }));
-            }
-        }
-    },
+}));
 
-    onUnload: function() {
-        for (let i = 0; i < patches.length; i++) patches[i]();
-        patches.length = 0;
-    }
-};
+const onUnload = () => patches.forEach(p => p ());
+
+exports.onUnload = onUnload;
