1b1dabd5419c86a7ef3c043c6e3a7e2924e2b465
femboytoucher504 <ahmemuhsins3169@gmail.com>
2026-06-14 13:49:23 +0300

    Add constants for emote and format configurations

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export const EMOTE_SIZE_DEFAULT = "48";
+export const FORMAT_TYPE_DEFAULT = "markdown";
+
+export const EMOTE_SIZE_KEY = "emote_size";
+export const FORMAT_TYPE_KEY = "format_type";
+export const REALMOJI_KEY = "realmoji";
+export const COMPOUND_SENTENCES_KEY = "compound_sentences";
+export const USE_WEBP_KEY = "use_webp";
+
+export const FORMAT_URL = "url";
+export const FORMAT_EXT_MD = "markdown_ext";
+export const FORMAT_MD = "markdown";
+
+export const REALMOJI_DEFAULT = false;
+export const COMPOUND_SENTENCES_DEFAULT = false;
+export const USE_WEBP_DEFAULT = false;
